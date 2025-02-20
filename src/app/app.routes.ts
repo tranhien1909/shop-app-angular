44728090875ff477277f001b16d90a1d29465f76
tranhien1909 <hient3565@gmail.com>
2025-02-20 15:55:41 +0700

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { DetailProductComponent } from './detail-product/detail-product.component';
 import { OrderComponent } from './order/order.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
-    { path: '', component: OrderComponent}
+    { path: '', component: RegisterComponent}
 ];
--- src/main.server.ts
+++ src/main.server.ts
@@ -3,7 +3,9 @@ import { HomeComponent } from './app/home/home.component';
 import { config } from './app/app.config.server';
 import { DetailProductComponent } from './app/detail-product/detail-product.component';
 import { OrderComponent } from './app/order/order.component';
+import { LoginComponent } from './app/login/login.component';
+import { RegisterComponent } from './app/register/register.component';
 
-const bootstrap = () => bootstrapApplication(OrderComponent, config);
+const bootstrap = () => bootstrapApplication(RegisterComponent, config);
 
 export default bootstrap;
